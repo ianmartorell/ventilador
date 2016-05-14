1675929dd2a535f88a93334b0995078cb94c7b02
Ian Martorell <ianmartorell@gmail.com>
2016-05-14 22:33:50 +0200

    Now using a timer interrupt

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,45 @@ int setPointPin = A1;
 int outputPin = 9;
 
 // Constants
-int sampleTime = 100;
+int sampleTime = 0.1; // in seconds
 
 // Working variables
-int input, output, outputPWM, setPoint;
+volatile int input, output, outputPWM, setPoint;
+volatile double errSum, lastErr;
 double kp, ki, kd;
-double errSum, lastErr;
-unsigned long lastTime;
 
 void setTunings(double a, double b, double c) {
   kp = a;
   ki = b * sampleTime;
   kd = c / sampleTime;
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  input = analogRead(inputPin);
+  setPoint = analogRead(setPointPin);
+
+  // P
+  double error = (double)(setPoint - input);
+  // I
+  errSum += error;
+  if (errSum > 1023) errSum = 1023;
+  else if (errSum < 0) errSum = 0;
+  // D
+  double dErr = (error - lastErr);
+
+  // compute output
+  output = kp * error + ki * errSum + kd * dErr;
+  if (output > 1023) output = 1023;
+  else if (output < 0) output = 0;
+
+  // map to PWM range
+  outputPWM = map((int)(output+0.5), 0, 1023, 0, 255);
+
+  analogWrite(outputPin, outputPWM);
+  Serial.println(outputPWM);
+
+  lastErr = error;
 }
 
 void setup()
@@ -26,40 +53,21 @@ void setup()
   pinMode(inputPin, INPUT);
   pinMode(setPointPin, INPUT);
   pinMode(outputPin, OUTPUT);
+
   setTunings(16.75, 0, 0);
+
+  noInterrupts();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  // set compare match register to desired timer count
+  OCR1A = (int) 16000000/(1024*(1/sampleTime))-1;
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // set CS10 and CS12 bits for 1024 prescaler
+  TCCR1B |= (1 << CS10);
+  TCCR1B |= (1 << CS12);
+  TIMSK1 |= (1 << OCIE1A);
+  interrupts();
 }
 
-void loop()
-{
-  unsigned long now = millis();
-  double timeChange = (double)(now - lastTime);
-
-  if (timeChange >= sampleTime)
-  {
-    input = analogRead(inputPin);
-    setPoint = analogRead(setPointPin);
-
-    // P
-    double error = (double)(setPoint - input);
-    // I
-    errSum += error;
-    if (errSum > 1023) errSum = 1023;
-    else if (errSum < 0) errSum = 0;
-    // D
-    double dErr = (error - lastErr);
-
-    // compute output
-    output = kp * error + ki * errSum + kd * dErr;
-    if (output > 1023) output = 1023;
-    else if (output < 0) output = 0;
-
-    // map to PWM range
-    outputPWM = map((int)(output+0.5), 0, 1023, 0, 255);
-
-    analogWrite(outputPin, outputPWM);
-    Serial.println(outputPWM);
-
-    lastErr = error;
-    lastTime = now;
-  }
-}
+void loop() {}
